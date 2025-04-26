a27ed820964f132233a015b0e2ab43526b1613c5
Swaran Nani <swaran-nani022@gmail.com>
2025-04-26 19:06:19 +0530

    Update build and deploy React app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/teacher-page/',  // Make sure this matches the name of your GitHub repository
+  base: '/teacher-page/',  // GitHub Pages subdirectory
   plugins: [react()],
-})
+});
