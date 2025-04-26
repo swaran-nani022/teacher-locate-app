32e4f182a77c3e31f1a8659a73921fe48731f683
Swaran Nani <swaran-nani022@gmail.com>
2025-04-26 18:47:31 +0530

    Fix paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/teacher-page/',  // Make sure this matches the name of your GitHub repository
   plugins: [react()],
 })
