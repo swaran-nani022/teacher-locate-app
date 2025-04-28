b66e5886c2566925cbb4a3faeb5d57520470a0a9
Swaran Nani <swaran-nani022@gmail.com>
2025-04-28 21:33:33 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/teacher-page/',  // <-- important
+  base: mode === 'development' ? '/' : '/teacher-locate-app/',  // Replace with your repo name
 }))
