f4f99db7b970103fc898faa9df2b873e8304c026
Swaran Nani <swaran-nani022@gmail.com>
2025-04-28 21:44:22 +0530

    fix vite.config for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isVercel = process.env.VERCEL === '1';  // Check if deployed on Vercel
-  return {
-    plugins: [react()],
-    base: mode === 'development' || isVercel ? '/' : '/teacher-locate-app/',  // Dynamically set base
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // For Vercel
 })
